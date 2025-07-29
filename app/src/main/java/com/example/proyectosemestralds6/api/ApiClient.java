e2308e90a92c1f790316872e1490b7a091fd9f0c
kevin <kevinmozhang@gmail.com>
2025-07-29 17:16:05 -0500

    ApiClient
    Clase singleton que configura Retrofit para hacer llamadas HTTP.

---
+++ app/src/main/java/com/example/proyectosemestralds6/api/ApiClient.java
@@ -0,0 +1,29 @@
+package com.example.proyectosemestralds6.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "http://tudominio.com/api/"; //el url remember
+    private static Retrofit retrofit = null;
+
+    public static ApiInterface getApiService() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiInterface.class);
+    }
+}
