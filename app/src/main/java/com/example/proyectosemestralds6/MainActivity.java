fc861da33956844c6ffce8e3877c3da60dcb67e7
kevin <kevinmozhang@gmail.com>
2025-07-29 17:56:26 -0500

    Update MainActivity.java
    falto agregar import view

--- app/src/main/java/com/example/proyectosemestralds6/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralds6/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.proyectosemestralds6;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.MenuItem;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
